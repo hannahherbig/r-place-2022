d21fbe39477ce873f6b808c52a9533f32d10a475
Hannah Herbig <notandrewherbig@gmail.com>
2022-04-02 18:00:40 -0400

    Fix position of oc.tc logo

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/959933935259877416/959933991983665152/overlay_v70.png";
+            i.src = "https://cdn.discordapp.com/attachments/959933935259877416/959935360455024670/overlay_v71.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
