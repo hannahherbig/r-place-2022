22ceb9c35ffde163921577ca41b596ccd6a65480
Hannah Herbig <notandrewherbig@gmail.com>
2022-04-03 11:35:25 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/959933935259877416/960191343655788594/overlay_v95.png";
+            i.src = "https://cdn.discordapp.com/attachments/960020069553942538/960200671678853120/overlay_v96.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
