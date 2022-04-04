c5fd88f9403e376e9be7716c862a1d78cd7f4943
Hannah Herbig <notandrewherbig@gmail.com>
2022-04-03 20:43:27 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://cdn.discordapp.com/attachments/959933935259877416/960324967118037023/overlay_full_04031948.png";
+        image.src = "https://cdn.discordapp.com/attachments/959933935259877416/960338591295373353/overlay_full_04032041.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
